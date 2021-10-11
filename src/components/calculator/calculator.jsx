e2298a1292e4ab7ef5a6c8acd4df96fbf53d50df
kokinz <nes-t@mail.ru>
2021-10-12 00:13:26 +0300

    fix

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
 function Calculator() {
+  const [creditTarget, setCreditTarget] = useState('');
+
+  const details = useRef();
+
+  const handleSelectorClick = (evt) => {
+    if (!details.current.open) {
+      details.current.open = true;
+      return;
+    }
+
+    details.current.open = false;
+    setCreditTarget(evt.target.title);
+  };
+
   return(
     <section className="calculator container">
       <h2 className="calculator__header">Кредитный калькулятор</h2>
@@ -9,22 +23,22 @@ function Calculator() {
         <fieldset className="calculator__fieldset calculator__fieldset--first-step">
           <legend className="calculator__legend">Шаг 1. Цель кредита</legend>
 
-          <details className="calculator__details">
+          <details className="calculator__details" ref={details}>
             <summary className="calculator__summary">
-              <input className="calculator__option-input" type="radio" name="item" id="default" title="Выберите цель кредита" checked />
+              <input className="calculator__option-input" type="radio" name="item" id="default" title="Выберите цель кредита" defaultChecked />
 
-              <input className="calculator__option-input" type="radio" name="item" id="item1" title="Ипотечное кредитование" />
+              <input className="calculator__option-input" type="radio" name="item" id="item1" title="Ипотечное кредитование" onClick={handleSelectorClick} />
 
-              <input className="calculator__option-input" type="radio" name="item" id="item2" title="Автомобильное кредитование" />
+              <input className="calculator__option-input" type="radio" name="item" id="item2" title="Автомобильное кредитование" onClick={handleSelectorClick} />
             </summary>
 
             <ul className="calculator__options list">
               <li className="calculator__option">
-                <label htmlFor="item1">Ипотечное кредитование</label>
+                <label className="calculator__label" htmlFor="item1">Ипотечное кредитование</label>
               </li>
 
               <li className="calculator__option">
-                <label htmlFor="item2">Автомобильное кредитование</label>
+                <label className="calculator__label" htmlFor="item2">Автомобильное кредитование</label>
               </li>
             </ul>
           </details>
